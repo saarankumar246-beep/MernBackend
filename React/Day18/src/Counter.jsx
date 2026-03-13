12d6f3197976d328ca3158a09674a2c2b37264c3
saarankumar246-beep <saarankumar246@gmail.com>
2026-03-13 11:01:29 -0700

    completed

---
+++ React/Day18/src/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
